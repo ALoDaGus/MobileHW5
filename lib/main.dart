035de43b185e3a3a5c61b861bafc9d81bc1b8747
ALoDaGus <thatchapol.to@gmail.com>
2021-08-08 22:57:25 +0700

    work

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import 'pages/game/game_page.dart';
+
+void main() {
+  
+  // var centerWidget = Center(child: myText());
+
+  //  runApp(centerWidget);
+
+   runApp(MyApp());
+}
+
+class myText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var rand = new Random().nextInt(100);
+
+    var textStyle = new TextStyle(fontSize: 127, color: Colors.green[300]);
+    return Text(
+        rand.toString(), 
+        textDirection: TextDirection.ltr,
+        style: textStyle,
+    );
+  }
+
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: GamePage(),
+    );
+  }
+
+}
---
+++ lib/pages/game/game_page.dart
@@ -0,0 +1,106 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class GamePage extends StatefulWidget {
+  const GamePage({Key? key}) : super(key: key);
+
+  @override
+  _GamePageState createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  int myNum = 0;
+
+  void _randNum() {
+    setState(() {
+        myNum = Random().nextInt(100);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _randNum();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GUESS THE NUMBER'),
+      ),
+      body: Container(
+        color: Colors.teal[200],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          // crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              color: Colors.teal[100],
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.person, // รูปไอคอน
+                    size: 80.0, // ขนาดไอคอน
+                    color: Colors.blue, // สีไอคอน
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'ธัชพล ออสุวรรณ',
+                        style: TextStyle(fontSize: 20, color: Colors.teal[700]),
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.star_rate, // รูปไอคอน
+                            size: 20.0, // ขนาดไอคอน
+                            color: Colors.teal[700], // สีไอคอน
+                          ),
+                          Icon(
+                            Icons.star_rate, // รูปไอคอน
+                            size: 20.0, // ขนาดไอคอน
+                            color: Colors.teal[700], // สีไอคอน
+                          ),
+                          Icon(
+                            Icons.star_rate, // รูปไอคอน
+                            size: 20.0, // ขนาดไอคอน
+                            color: Colors.teal[700], // สีไอคอน
+                          ),
+                          Icon(
+                            Icons.star_rate, // รูปไอคอน
+                            size: 20.0, // ขนาดไอคอน
+                            color: Colors.teal[700], // สีไอคอน
+                          ),
+                          Icon(
+                            Icons.star_half, // รูปไอคอน
+                            size: 20.0, // ขนาดไอคอน
+                            color: Colors.teal[700], // สีไอคอน
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              myNum.toString(),
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 177, color: Colors.teal),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    _randNum();
+                  },
+                  child: Text('RANDOM'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
