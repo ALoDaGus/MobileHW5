92c2fe73977764ef1db5a9928029ce4e52f608f0
ALoDaGus <thatchapol.to@gmail.com>
2021-09-07 18:41:43 +0700

    HW8

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter/material.dart';
 
-import 'pages/game/hw6game_page.dart';
+import 'pages/game/game_page.dart';
 
 void main() {
   
--- lib/pages/game/game_page.dart
+++ lib/pages/game/game_page.dart
@@ -1,4 +1,4 @@
-import 'dart:math';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'package:flutter/material.dart';
 
@@ -10,83 +10,55 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
-  int? myNum;
+  String currentFont = 'Prompt';
+  List<String> fontName = ['Prompt', 'Kanit', 'Taviraj', 'Sriracha', 'Pridi', 'Mali', 'Itim', 'Charm', 'Pattaya', 'Chonburi'];
 
-  void _randNum() {
-    setState(() {
-        myNum = Random().nextInt(100);
-    });
-  }
+  Widget _button(font) => Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: ElevatedButton(
+        onPressed: () {
+          setState(() => currentFont = font);
+        },
+        child: Text(
+          font,
+          style: GoogleFonts.getFont(font),
+        )),
+  );
 
   @override
   Widget build(BuildContext context) {
-    _randNum();
-    List<Widget> iconList = [];
-    
-    for(var i = 0; i < 4; i++) {
-      iconList.add(Icon(Icons.star, color: Colors.orange));
-    }
-    iconList.add(Icon(Icons.star_border_outlined, color: Colors.orange));
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text('GUESS THE NUMBER'),
-      ),
+      appBar: AppBar(title: const Text('THAI FONT VIEWER')),
       body: Container(
-        color: Colors.teal[200],
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(7.0),
-                // color: Colors.teal[100],
-                gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [Colors.teal[200]!, Colors.lightGreen[300]!, Colors.red[300]!],
+        color: Colors.teal[100],
+        child: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Center(
+                    child: Text(
+                      'การเดินทางของฉันและเธอคือการเรียนรู้ การเรียนรู้ของเราสองคนคือความเข้าใจ ~',
+                      style: GoogleFonts.getFont(currentFont, fontSize: 50.0),
+                    ),
+                  ),
                 ),
               ),
-              margin: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.person, 
-                    size: 80.0,
-                    color: Colors.blue,
+              Text('Font ' + currentFont, style: GoogleFonts.getFont(currentFont),),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  color: Colors.teal[50],
+                  elevation: 5.0,
+                  child: Wrap(
+                      alignment: WrapAlignment.center,
+                    children: fontName.map((e) => _button(e)).toList(),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'ธัชพล ออสุวรรณ',
-                        style: TextStyle(fontSize: 20, color: Colors.teal[800], fontWeight: FontWeight.w600),
-                      ),
-                      Row(
-                        children: iconList,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Text(
-              myNum.toString(),
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 177, color: Colors.teal),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                width: double.infinity,
-                height: 40,
-                child: ElevatedButton(
-                  onPressed: _randNum,
-                  child: Text('RANDOM'),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
