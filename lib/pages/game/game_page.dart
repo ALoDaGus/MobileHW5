cb1b94ca71ef8cc3fb88f1e1fa6c0eb2d8856e36
ALoDaGus <thatchapol.to@gmail.com>
2021-09-14 20:19:53 +0700

    lec9

--- lib/pages/game/game_page.dart
+++ lib/pages/game/game_page.dart
@@ -1,6 +1,9 @@
-import 'package:google_fonts/google_fonts.dart';
+import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'game.dart';
 
 class GamePage extends StatefulWidget {
   const GamePage({Key? key}) : super(key: key);
@@ -10,55 +13,107 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
-  String currentFont = 'Prompt';
-  List<String> fontName = ['Prompt', 'Kanit', 'Taviraj', 'Sriracha', 'Pridi', 'Mali', 'Itim', 'Charm', 'Pattaya', 'Chonburi'];
+  late Game _game;
+  final TextEditingController _controller = TextEditingController();
+  String? _guessNumber;
+  String? _feedback;
 
-  Widget _button(font) => Padding(
-    padding: const EdgeInsets.all(8.0),
-    child: ElevatedButton(
-        onPressed: () {
-          setState(() => currentFont = font);
-        },
-        child: Text(
-          font,
-          style: GoogleFonts.getFont(font),
-        )),
-  );
+  @override
+  void initState() {
+    super.initState();
+    _game = Game();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('THAI FONT VIEWER')),
       body: Container(
-        color: Colors.teal[100],
+        color: Colors.yellow.shade100,
         child: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Center(
-                    child: Text(
-                      'การเดินทางของฉันและเธอคือการเรียนรู้ การเรียนรู้ของเราสองคนคือความเข้าใจ ~',
-                      style: GoogleFonts.getFont(currentFont, fontSize: 50.0),
-                    ),
-                  ),
-                ),
-              ),
-              Text('Font ' + currentFont, style: GoogleFonts.getFont(currentFont),),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Card(
-                  color: Colors.teal[50],
-                  elevation: 5.0,
-                  child: Wrap(
-                      alignment: WrapAlignment.center,
-                    children: fontName.map((e) => _button(e)).toList(),
-                  ),
-                ),
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _buildHeadder(),
+                _buildMainContent(),
+                _buildInputPanel(),
+              ],
+            ),
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeadder() {
+    return Column(
+      children: [
+        Image.asset(
+          'assets/images/logo_number.png',
+          width: 240.0, // 160 = 1 inch
+        ),
+        Text(
+          'GUESS THE NUMBER',
+          style: GoogleFonts.kanit(fontSize: 22.0),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMainContent() {
+    return _guessNumber == null ? SizedBox.shrink() : Column(
+      children: [
+        Text(_guessNumber!),
+        Text(_feedback!),
+      ],
+    );
+  }
+
+  Widget _buildInputPanel() {
+    return Card(
+      color: Colors.teal[100],
+      elevation: 10.0,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Flexible(
+              child: TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.teal, width: 10.0),
+                  ),
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () => setState(() {
+                _guessNumber = _controller.text;
+                int? guess = int.tryParse(_guessNumber!);
+    
+                if (guess != null) {
+                  var result = _game.doGuess(guess);
+                  if (result == 0) {
+                    _feedback = 'CORRECT!';
+                  } else if (result == 1) {
+                    _feedback = 'TOO HIGH!';
+                  } else {
+                    _feedback = 'TOO LOW';
+                  }
+                }
+              }),
+              child: Text('GUESS'),
+            ),
+          ],
         ),
       ),
     );
